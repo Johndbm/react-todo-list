9602dfe40a42c5ac36a5c5a9d3158cbbde73045f
john.banks <johndbanksm@gmail.com>
2022-12-07 18:47:21 +0000

    added form+tasks logics + footer tasks lenght

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,118 @@
-import React from "react";
+import React, {useState} from "react";
+// import Todos from "../view/Todos.jsx";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
+
 //create your first component
 const Home = () => {
+
+	const [task, setTask] = useState(
+		{
+		label:"", 
+		done: false,
+		}
+	);
+
+	const [allTask, setAllTask] = useState([
+		{
+			label: "Comprar Pan",
+			done: false,
+		},
+		{
+			label: "Comprar Verduras",
+			done: false,
+		},
+		{
+			label: "Comprar Perrarina",
+			done: false,
+		},
+	]);
+
+	const handleTask = (event) => {
+		
+		setTask({
+			...task,
+			[event.target.name]: event.target.value
+		})
+		
+
+	};
+
+	const addTask = (event) => {
+		
+		if (event.key == "Enter") {
+			setAllTask([
+				task, ...allTask
+			])
+		}
+	} 
+
+	const delTask = (id) => {
+		console.log(id)
+		const filterTask = allTask.filter((item, index) => index != id)
+		console.log(filterTask)
+		setAllTask(
+			filterTask
+		)
+
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row justify-content-center">
+				<div className="display-1 text-center">"To-Do list"</div>
+				<div className="">
+					<input 
+							className="form-control" 
+							type="text" 
+							value={task.label}
+							name="label"
+							placeholder="What needs to be done? (Type-in a task)" 
+							onKeyDown={addTask} 
+							onChange={handleTask}
+						/>
+					<ul className="list-group list-group-flush">
+						{allTask.map( (item, index) =>
+						(
+							<li 
+								className="list-group-item" 
+								onClick={()=> delTask(index)}>
+									{item.label}
+							</li>
+						))}
+						
+					</ul>
+					<div className="card-footer fw-lighter fst-italic text-secondary">
+						"{allTask.length}" task(s) left to-do
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
+
+
+/* Bootstrap Card Template "Discardable" 
+
+<div className="card" style="width: 18rem;">
+  <div className="card-header">
+    "To-Do list"
+  </div>
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">1 item</li>
+    <li className="list-group-item">2 item</li>
+    <li className="list-group-item">3 item</li>
+  </ul>
+  <div className="card-footer">
+    "n" item(s) left
+  </div>
+</div>
+
+*/
